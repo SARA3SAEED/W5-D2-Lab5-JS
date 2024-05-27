689e9cfcc23ff5c10b47838ea01b6482cab21af2
SARA3SAEED <Swaretoo@gmail.com>
2024-05-27 20:29:27 +0300

    first commit

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 
 let bill = document.getElementById('inputBill');
 let count = document.getElementById('inputCount');
+
 let btn = document.getElementById('btn');
+let btn2 = document.getElementById('btn2');
+
 let result = document.getElementById('result');
+let result2 = document.getElementById('result2');
+
 
 
 btn.addEventListener('click', function() {
-    result.innerText = bill.value * 0.15 / count.value;
+    result.innerText = bill.value * 0.15;
+
+})
+
+btn2.addEventListener('click', function() {
+    result2.innerText = (bill.value - bill.value*0.15 )/ count.value;
 });
 
